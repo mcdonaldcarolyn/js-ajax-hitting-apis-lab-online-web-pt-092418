511a822b97cb411d0a678e746b02d18b7789eeb3
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-05-09 12:08:27 -0400

    Interim checkin

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // your code here
+function getRepositories() {
+   const username = document.querySelector('#username').value;
+      const url = `https://api.github.com/users/${username}/repos`;
+  const req = new XMLHttpRequest();
+  eq.addEventListener('load', function () {
+            // obviously do something else with the response text than just printing it out
+            console.log(this.responseText);
+        });
+    
+        req.open('GET', url);
+        req.send();
+}
